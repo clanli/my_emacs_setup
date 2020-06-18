811372b5738083bbbef79a999ac1327f4c24e13a
clanli <claes.lindvall@icloud.com>
2020-06-18 10:38:50 +0200

    Create simple_init.el

---
+++ simple_init.el
@@ -0,0 +1,45 @@
+;; init.el --- Emacs configuration
+
+;; INSTALL PACKAGES
+;; --------------------------------------
+
+
+;; Installera följande på hosten
+;; # apt install flake8 python-pip elpa-py-autopep8 python-autopep8
+;; # pip install jedi
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(dirtree))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves/"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+
+;; (set-cursor-color "#fff0f5")
+(setq default-frame-alist
+      '((cursor-color . "palegoldenrod")))
+
+(setq inhibit-startup-message t) ;; hide the startup message
+;;(load-theme 'material t) ;; load material theme
+(global-linum-mode t) ;; enable line numbers globally
